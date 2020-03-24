4cdf9a0985093a29b0ff5cf2202f33fb97ff8ad5
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2020-03-24 00:11:02 -0700

    adds appConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {InjectionToken, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NotificationComponent } from './notification/notification.component';
+
+export interface AppConfigInterface {
+  EventSourceUrl: string;
+}
+
+export const APP_CONFIG = new InjectionToken<AppConfigInterface>('APP_CONFIG');
+
+export const AppConfig: AppConfigInterface = {
+  EventSourceUrl: ''
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,9 @@ import { NotificationComponent } from './notification/notification.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_CONFIG, useValue: AppConfig}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
