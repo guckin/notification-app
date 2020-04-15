90b69d92be4be92e027fd6053f96b3a06d1607e6
stephenmcguckin <stephen.mcguckin@dat.com>
2020-04-15 01:07:02 -0700

    adds a popup for the notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {NotificationComponent} from './notification/notification.component';
+import {NotificationComponent, NotificationPopUpComponent} from './notification/notification.component';
 import {APP_CONFIG, AppConfig} from './app-config';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     declarations: [
+        NotificationPopUpComponent,
         NotificationComponent,
         AppComponent
     ],
+    entryComponents: [NotificationComponent, NotificationPopUpComponent],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatButtonModule
     ],
     providers: [
         {provide: APP_CONFIG, useValue: AppConfig}
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Notification, NotificationService} from '../notification.service';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
     selector: 'app-notification',
@@ -7,17 +8,45 @@ import {Notification, NotificationService} from '../notification.service';
     styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
-    currentNotification: Notification;
 
-    constructor(private readonly notificationService: NotificationService) {
+    private dialogVisible = false;
+    private currentDialog: MatDialogRef<NotificationPopUpComponent, Notification>;
+
+    constructor(private readonly notificationService: NotificationService,
+                private readonly dialog: MatDialog) {
     }
 
     ngOnInit() {
         this.notificationService
             .notificationSubscription()
-            .subscribe((notification) => {
-                this.currentNotification = notification;
+            .subscribe(notification => {
+                if (this.dialogVisible) {
+                    this.currentDialog.close();
+                }
+                this.currentDialog = this.dialog.open(NotificationPopUpComponent, {
+                    width: '250px',
+                    data: notification
+                });
+                this.dialogVisible = true;
+                this.currentDialog.afterClosed().subscribe(() => {
+                    this.dialogVisible = false;
+                });
             });
+    }
+
+}
+
+@Component({
+    selector: 'app-notification-popup',
+    templateUrl: './notification-popup.component.html',
+})
+export class NotificationPopUpComponent {
+
+    constructor(public dialogRef: MatDialogRef<NotificationPopUpComponent>,
+                @Inject(MAT_DIALOG_DATA) public notification: Notification) {}
+
+    onNoClick(): void {
+        this.dialogRef.close();
     }
 
 }
